1d4f3e3594d335a12b0124f6611627e44948b4c8
vivdev2 <vivekmahajan256@gmail.com>
2025-09-10 22:32:37 +0530

    phase1

--- app/page.tsx
+++ app/page.tsx
@@ -34,6 +34,14 @@ export default function Page() {
         {view === 'calendar' && <CalendarView />}
         {view === 'timeline' && <TimelineView />}
       </main>
+
+    
+
+
+
+
+
     </div>
+
   );
 }
