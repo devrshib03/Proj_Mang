bf1804965cc1e33b268250ea61d9cabe9018b6a3
shubham maurya <sm7758525@gmail.com>
2025-09-18 22:59:26 +0530

    modification in landing page

---
+++ app/features/page.tsx
@@ -0,0 +1,15 @@
+import Header from "components/Header";
+import Footer from "components/Footer";
+import Features from "components/Features";
+
+export default function FeaturesPage() {
+  return (
+    <main className="min-h-screen flex flex-col">
+      <Header />
+      <Features />
+      <Footer />
+    </main>
+  );
+}
+
+
---
+++ app/pricing/page.tsx
@@ -0,0 +1,15 @@
+import Header from "components/Header";
+import Footer from "components/Footer";
+import PricingSection from "components/PricingSection";
+
+export default function PricingPage() {
+  return (
+    <main className="min-h-screen flex flex-col">
+      <Header />
+      <PricingSection />
+      <Footer />
+    </main>
+  );
+}
+
+
